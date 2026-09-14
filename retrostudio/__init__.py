758bb494355b31eb88bda6bc878b088e165ff135
pos <pgousdal@users.noreply.github.com>
2026-09-14 17:32:24 +0200

    M2: export stable target API

--- retrostudio/__init__.py
+++ retrostudio/__init__.py
@@ -1,13 +1,31 @@
-"""RetroStudio core project model."""
+"""RetroStudio platform-neutral core API."""
 
 from .model import Component, Diagnostic, Entity, Project, Scene, load_project, save_project
+from .target import (
+    TARGET_API_VERSION,
+    BackendDescriptor,
+    BackendResult,
+    BuildArtifact,
+    Capability,
+    TargetBackend,
+    load_backend,
+    negotiate_backend,
+)
 
 __all__ = [
+    "BackendDescriptor",
+    "BackendResult",
+    "BuildArtifact",
+    "Capability",
     "Component",
     "Diagnostic",
     "Entity",
     "Project",
     "Scene",
+    "TARGET_API_VERSION",
+    "TargetBackend",
+    "load_backend",
     "load_project",
+    "negotiate_backend",
     "save_project",
 ]
